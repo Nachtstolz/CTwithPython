be386ec97785ac2a108d66586a8042c399c151d5
Nachtstolz <sayong4424@gmail.com>
2024-01-25 23:56:19 +0900

    240125 :: this question added solution in the book is pushed

--- q29.py
+++ q29.py
@@ -37,3 +37,43 @@ while min_gap <= max_gap : # 이진 탐색
         min_gap = mid+1
 
 print(res)
+
+''' 교재 솔루션 코드 '''
+# 이진 탐색으로 '가장 인접한 두 공유기 사이 거리'를 조절해가며, 매 순간 실제로 공유기를 설치하여
+# C보다 많은 개수로 공유기를 설치할 수 있는지 체크해 문제를 해결할 수 있다.
+# 다만, '가장 인접한 두 공유기 사이 거리'의 최댓값을 찾아야 하므로, C보다 많은 개수로 공유기를 설치할 수 있다면
+# '가장 인접한 두 공유기 사이 거리'의 값을 증가시켜 더 큰 값에 대해서도 성립하는지 체크하기 위해 재탐색한다.
+# 이진 탐색을 이용해 해결할 수 있는 ⭐️파라메트릭 서치 유형⭐️의 문제로 이해 가능하다.
+
+'''
+# 집의 개수(N)와 공유기의 개수(C)를 입력받기
+n, c = list(map(int, input().split(' ')))
+
+# 전체 집의 좌표 정보를 입력받기
+array = []
+for _ in range(n) :
+    array.append(int(input()))
+array.sort() # 이진 탐색 수행을 위해 정렬
+
+start = array[1] - array[0] # 집의 좌표 중 가장 작은 값
+end = array[-1] - array[0] # 집의 좌표 중 가장 큰 값
+result = 0
+
+while(start <= end):
+    mid = (start+end) // 2 # mid는 가장 인접한 두 공유기 사이 거리(gap) 의미
+    value = array[0]
+    count = 1
+    # 현재의 mid값을 이용해 공유기 설치
+    for i in range(1, n) : # 앞에서부터 차근차근 설치
+        if array[i] >= value+mid :
+            value = array[i]
+            count+=1
+
+    if count>=c : # C개 이상의 공유기를 설치할 수 있는 경우, 거리 증가
+        start = mid+1
+        result = mid # 최적의 결과 저장
+    else : # C개 이상의 공유기를 설치할 수 없는 경우, 거리 감소
+        end = mid-1
+
+print(result)
+'''
