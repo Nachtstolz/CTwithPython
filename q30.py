9d923f21a52e9fac939a95dcd1d6090263ddaa99
Nachtstolz <sayong4424@gmail.com>
2024-04-13 02:30:29 +0900

    240413 :: the problem q30 is updated by solution codes on textbook - you should read the footnotes about the idea of sol

--- q30.py
+++ q30.py
@@ -26,7 +26,7 @@
     # - 예를 들어 "??odo", "fro??", "?????"는 가능한 키워드
     # - 반면에 "frodo"(?가 없음), "fr?do"(?가 중간에 있음), "?ro??"(?가 양쪽에 있음)는 불가능 키워드
 
-def solution(words, queries) :
+def solution2(words, queries) :
     answer = []
 
     for search in queries :
@@ -81,3 +81,60 @@ def solution(words, queries) :
 # words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
 # queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
 # solution(words, queries)
+
+''' 교재 풀이 '''
+
+# 각 단어를 길이에 따라 나눈다. 이후 모든 리스트 정렬 -> 각 쿼리에 대해 이진 탐색 수행
+# 예를 들어, 문제의 예시와 같이 전체 단어가 ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+# 로 구성되어 있을 때 길이에 따라 나눈 후 정렬하면 아래와 같다.
+# 길이가 5인 단어 리스트 : ["frame", "frodo", "front", "frost", "kakao"]
+# 길이가 6인 단어 리스트 : ["frozen"]
+# 이제 "fro??"라는 쿼리가 들어왔다고 가정하면, "fro??"는 길이가 5이므로 길이가 5인 단어 리스트에서 "fro"로 시작하는 
+# 모든 단어를 찾으면 된다. 이때 구체적으로 이진 탐색을 이용해 "fro"로 시작되는 마지막 단어의 위치를 찾고, "fro"로 시작하는
+# 첫 단어의 위치를 찾아 그 위치의 차이를 계산하면 될 것이다. 이처럼 이진 탐색으로 특정 단어가 등장한 횟수를 게산할 수 있다.
+
+# 혹은 "fro??"라는 쿼리가 들어왔을 때, 부록에서 다루었던 count_by_range() 함수를 이용하여 "froaa"보다 크거나 같으면서
+# "frozz"보다 작거나 같은 단어의 개수를 세도록 구현하면 매우 간단하다.
+
+# 다만, 문제에서는 와일드카드 "?"가 접두사에서도 등장할 수 있다고 하였다. 만약 "????o"라는 쿼리가 들어왔다고 가정하자.
+# 이는 기존의 리스트인 ["frame", "frodo", "front", "frost", "kakao"]를 이용해서 처리할 수가 없을 것이다.
+# 따라서 접두사에 와일드카드가 등장하는 것을 처리하기 위해 기존 단어를 뒤집은 단어를 담고 있는 리스트 또한 별도 선언해야 한다.
+# 현재 예시에서는 길이가 5인 뒤집힌 단어 리스트는 ["emarf", "oakak", "odorf", "tnorf", "tsorf"]이다.
+# 결과적으로 접두사에 와일드카드가 등장하는 경우, 뒤집힌 단어 리스트를 대상으로 이진 탐색을 수행하면 된다.
+
+from bisect import bisect_left, bisect_right
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+# 앞서 설명한대로 right_value - left_value 인덱스로 계산도 가능
+def count_by_range(a, left_value, right_value) :
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
+# 모든 단어를 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries) :
+    answer = []
+    for word in words : # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
+        array[len(word)].append(word) # 단어 삽입
+        reversed_array[len(word)].append(word[::-1]) # 단어를 뒤집어서 삽입
+
+    for i in range(10001) : # 이진 탐색을 수행하기 위해 각 단어 리스트 정렬 수행
+        array[i].sort()
+        reversed_array[i].sort()
+
+    for q in queries : # 쿼리를 하나씩 확인하며 처리
+        if q[0] != '?' : # 접미사에 와일드카드가 붙은 경우
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else : # 접두사에 와일드카드가 붙은 경우
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        # 검색된 단어의 개수 저장
+        answer.append(res)
+    return answer
+
+# words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+# queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
+# print(solution(words, queries))
